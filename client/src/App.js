c3693ef5611c117b6b1a3cc24f288278347d23ec
Matthew Weigand <mweigand@dev-10.com>
2020-11-10 17:12:34 -0600

    client react app creation/create season page creation

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import TeamsCreation from './TeamsCreation';
+
+function App() {
+  return (
+    <TeamsCreation></TeamsCreation>
+  );
+}
+
+export default App;
---
+++ client/src/TeamsCreation.js
@@ -0,0 +1,120 @@
+import React from "react";
+
+
+class TeamsCreation extends React.Component {
+    constructor() {
+
+        super();
+        this.state = {
+            teams: [],
+            numberOfTeams: 0,
+            id: 0,
+            teamName: '', //not sure, but including for now
+            numberOfGames: 0,
+            userTeamChoiceId: 0,
+        };
+    }
+
+    getTeams = () => {
+        fetch("http://localhost:8080/teams")
+            .then((response) => response.json())
+            .then((data) => {
+                this.setState({
+                    teams: data,
+                    id: 0,
+                    teamName: '',
+                })
+
+            });
+    };
+
+    componentDidMount() {
+        this.getTeams();
+    }
+
+    changeHandler = (event) => {
+
+        const target = event.target;
+        const value = target.value;
+        const numberOfTeams = target.name;
+        const teamName = target.name
+        const userTeamChoiceId = target.name
+
+        this.setState({
+            [numberOfTeams]: value,
+            [teamName]: value,
+            [userTeamChoiceId]: value
+        });
+    };
+
+
+    createSeasonHandler = (event) => {
+        event.preventDefault();
+
+        fetch(`http://localhost:8080/teams`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                numberOfTeams: this.state.numberOfTeams,
+                numberOfGames: this.state.numberOfGames,
+                userTeamChoiceId: this.state.userTeamChoiceId
+            }),
+        }).then((response) => {
+            if (response.status === 201) {
+                console.log("Success!");
+                response.json().then((data) => console.log(data));
+                this.getTeams();
+            } else if (response.status === 400) {
+                console.log("Errors!");
+                response.json().then((data) => console.log(data));
+            } else {
+                console.log("oops...");
+            }
+        });
+    };
+
+
+    render() {
+        return (
+            <>
+                <h2 className="text-center"> Create Season!</h2><br /><br />
+
+                <form>
+
+                    <div className="form-group col-6">
+                        <label for="selectNumberOfTeams">Enter the number of teams you to create in your league</label>
+                        <input type="number" className="form-control" name="numberOfTeams" value={this.state.numberOfTeams}
+                            onChange={this.changeHandler} />
+                    </div>
+
+                    <div className="form-group" class="col-6">
+                        <label for="selectNumberOfGames">Enter the number of games to play in the season</label>
+                        <input type="number" className="form-control" name="numberOfGames" value={this.state.numberOfGames}
+                            onChange={this.changeHandler} />
+                    </div><br /><br />
+
+                </form>
+
+                <div className="dropdown">
+                    <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Choose your Team
+                    </button>
+                    <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                        <button className="dropdown-item" href="#">Chicago Cubs</button>
+                        <button className="dropdown-item" href="#">Cincinatti Reds</button>
+                        <button className="dropdown-item" href="#">Chicago White Sox</button>
+                    </div>
+                </div><br /><br />
+
+                <div>
+                    <button type="submit" class="btn btn-block btn-dark">Create Season</button>
+                </div>
+
+            </>
+        );
+    }
+}
+export default TeamsCreation;
+
