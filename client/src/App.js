e48ccb53afbcd9fcdaac1b00b8c3f0e9f35b66a3
Matthew Weigand <mweigand@dev-10.com>
2020-11-16 20:08:30 -0600

    page for season, view standings, and season schedule added

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-import Draft from './Draft';
+import ViewSchedule from './ViewSchedule';
 
 function App() {
   return (
-    <Draft></Draft>
+    <ViewSchedule></ViewSchedule>
   );
 }
 
