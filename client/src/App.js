cf70025000fccff992264beff51a683ce2131d31
charIiemurphy <38874273+charIiemurphy@users.noreply.github.com>
2020-11-18 16:50:33 -0600

    added security components

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
+import React, { useState } from 'react';
+import jwt_decode from 'jwt-decode';
+
 import './App.css';
-import React from 'react';
+//import React from 'react';
 import Draft from './Draft';
 import ViewStandings from './ViewStandings';
 import ViewSchedule from './ViewSchedule';
@@ -7,43 +10,94 @@ import PlaySeason from './PlaySeason';
 import TeamsCreation from './TeamsCreation';
 import SeasonFacts from './SeasonFacts';
 import ViewFreeAgents from './ViewFreeAgents';
+import Login from './Login'
+import AuthContext from './AuthContext';
 
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
+  Link,
+  Redirect
 } from "react-router-dom";
 
+function Users() {
+  return <h1>Users</h1>;
+}
 
+function NotFound() {
+  return <h1>Not Found</h1>;
+}
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  const login = (token) => {
+
+    const { appUserId, sub: username, authorities } = jwt_decode(token);
+
+    // Split the authorities into an array of roles.
+    const roles = authorities.split(',');
+  
+    const user = {
+      appUserId: parseInt(appUserId, 10),
+      username,
+      roles,
+      token,
+      hasRole(role) {
+        return this.roles.includes(role);
+      }
+    };
+  
+    console.log(user);
+
+    setUser(user);
+
+    return user;
+  };
+
+  const logout = () => {
+    setUser(null);
+  };
+
+  // Create the auth object
+  const auth = {
+    user,
+    login,
+    logout
+  };
+
   return (
-    <Router>
-      <Switch>
-        <Route path="/SeasonFacts">
-          <SeasonFacts />
-        </Route>
-        <Route path="/PlaySeason">
-          <PlaySeason />
-        </Route>
-        <Route path="/ViewSchedule">
-          <ViewSchedule />
-        </Route>
-        <Route path="/ViewStandings">
-          <ViewStandings />
-        </Route>
-        <Route path="/ViewFreeAgents">
-          <ViewFreeAgents />
-        </Route>
-        <Route path="/draft">
-          <Draft />
-        </Route>
-        <Route path="/">
-          <TeamsCreation />
-        </Route>
-      </Switch>
-    </Router>
+    <AuthContext.Provider value={auth}>
+      <Router>
+        <Switch>
+          <Route path="/SeasonFacts">
+            <SeasonFacts />
+          </Route>
+          <Route path="/PlaySeason">
+            <PlaySeason />
+          </Route>
+          <Route path="/ViewSchedule">
+            <ViewSchedule />
+          </Route>
+          <Route path="/ViewStandings">
+            <ViewStandings />
+          </Route>
+          <Route path="/ViewFreeAgents">
+            <ViewFreeAgents />
+          </Route>
+          <Route path="/draft">
+            <Draft />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/">
+            <TeamsCreation />
+          </Route>
+        </Switch>
+      </Router>
+    </AuthContext.Provider>
   );
 }
 
---
+++ client/src/Login.js
@@ -0,0 +1,66 @@
+import React, { useState, useContext } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+
+//import AuthContext from './AuthContext';
+import Errors from './Errors';
+
+export default function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState([]);
+
+  //const auth = useContext(AuthContext);
+  const history = useHistory();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const response = await fetch('http://localhost:8080/authenticate', {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        username,
+        password
+      })
+    });
+    
+    if (response.status === 200) {
+     // const { jwt_token } = await response.json();
+    
+      // {
+      //   "jwt_token": "eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJjYWxvcmllLXRyYWNrZXIiLCJzdWIiOiJzbWFzaGRldjUiLCJhdXRob3JpdGllcyI6IlJPTEVfVVNFUiIsImV4cCI6MTYwNTIzNDczNH0.nwWJtPYhD1WlZA9mGo4n5U0UQ3rEW_kulilO2dEg7jo"
+      // }
+    
+      //auth.login(jwt_token);
+    
+      history.push('/');
+    } else if (response.status === 403) {
+      setErrors(['Login failed.']);
+    } else {
+      setErrors(['Unknown error.']);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <Errors errors={errors} />
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Username:</label>
+          <input type="text" onChange={(event) => setUsername(event.target.value)} />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input type="password" onChange={(event) => setPassword(event.target.value)} />
+        </div>
+        <div>
+          <button type="submit">Login</button>
+          <Link to="/register">I don't have an account</Link>
+        </div>
+      </form>
+    </div>
+  );
+}
